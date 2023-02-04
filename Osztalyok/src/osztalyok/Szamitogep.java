9e8cabd0d2a85a0df337ca180821032ddf217abc
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-02-04 20:26:07 +0100

    számítógép osztály bővítése metódusokkal

--- Osztalyok/src/osztalyok/Szamitogep.java
+++ Osztalyok/src/osztalyok/Szamitogep.java
@@ -4,11 +4,16 @@ package osztalyok;
 /**
  *
  * @author Székely Balázs
+ * 
+ * //2. feladat - Számítógép osztály megvalósítása
  */
 public class Szamitogep {
+    
+    //adattagok
     private double szabadMemoria;
     private boolean bekapcsolva;
 
+    //konstruktorok
     public Szamitogep(double szabadMemoria, boolean bekapcsolva) {
         this.szabadMemoria = szabadMemoria;
         this.bekapcsolva = bekapcsolva;
@@ -17,6 +22,30 @@ public class Szamitogep {
     public Szamitogep() {
         this(1024, false);
     }
+    
+    //metódusok
+
+    @Override
+    public String toString() {
+        return "A számítógép szabad memóriája: "+this.szabadMemoria+"MB és jelenleg "+(this.bekapcsolva? "be van kapcsolva":"nincs bekapcsolva");
+    }
+    
+    
+    
+    public void kapcsol(){
+        if(this.bekapcsolva) this.bekapcsolva = false;
+        else this.bekapcsolva = true;
+    }
+    
+    public boolean programMasol(double programMeret){
+        boolean siker = false;
+        if(this.bekapcsolva && (this.szabadMemoria >= programMeret)){
+            this.szabadMemoria -= programMeret;
+            siker = true;
+        }
+        return siker;
+    }
+    
     
     
 }
